ad764191803fe08d6acf37ef65bfbde0ba27fb19
tamaskornel <113334847+tamaskornel@users.noreply.github.com>
2023-09-25 14:10:32 +0200

    Add files via upload

---
+++ sajat_feladat.py
@@ -0,0 +1,19 @@
+
+#Kérem az első számot: 15
+#Kérem a második számot: 3
+#A két szám összege: 18
+#A két szám különbsége: 12
+#A két szám szorzata: 45
+#A két szám hányadosa: 5
+
+szam1=int(input("Kérem az első számot: "))
+szam2=int(input("Kérem a második számot: "))
+osszeg=szam1+szam2
+kulonbseg=szam1-szam2
+szorzat=szam1*szam2
+hanyados=szam1/szam2
+
+print("A két szám összege:",osszeg)
+print("A két szám különbsége:",kulonbseg)
+print("A két szám szorzata:",szorzat)
+print("A két szám hányadosa:",hanyados)
